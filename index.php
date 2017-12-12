3632ee42f082588603b69d94f02d7041b552c872
Ein Andersson <einand@3gdev.com>
2017-12-12 17:23:17 +0100

    first poc

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: einandersson
+ * Date: 2017-12-12
+ * Time: 16:35
+
+Plugin Name: Visitor Status for CIlamp
+Plugin URI:  http://www.cilamp.se
+Description: Change color if anyone visits your site
+Version:     0.0.1
+Author:      Ein Andersson on demand of Olof Bjarnarson
+Author URI:  http://www.cilamp.se
+License:     FreeBSD
+License URI: https://www.freebsd.org/copyright/freebsd-license.html
+Domain Path:
+Text Domain: cilamp
+*/
+
+
+add_action('init', 'cilamp_init');
+
+function cilamp_init() {
+    wp_enqueue_script('visitor-cilamp', plugin_dir_url( __FILE__ ). 'js/script.js', array( 'jquery' ), false, true);
+    add_action( 'wp_ajax_cilamp_ajax_action', 'cilamp_ajax_action' );
+    wp_localize_script( 'visitor-cilamp', 'cilamp_ajax',
+        array( 'ajax_url' => admin_url( 'admin-ajax.php' ) ) );
+}
+
+
+function cilamp_ajax_action() {
+    $colors = ['FF0000', '00FF00', '0000FF'];
+    global $wpdb; // this is how you get access to the database
+
+    $color =  $colors[rand(0,2)];
+    $ch = curl_init();
+
+    curl_setopt($ch, CURLOPT_URL,"https://api.cilamp.se/v1/testsystem/");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS,'color='. $color );
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $server_output = curl_exec ($ch);
+
+    curl_close ($ch);
+
+    echo $color . PHP_EOL;
+    if ($server_output != "OK") {
+        echo $server_output . PHP_EOL;
+    } else {
+        echo 'error' . PHP_EOL;
+    }
+
+
+    wp_die(); // this is required to terminate immediately and return a proper response
+}
---
+++ js/script.js
@@ -0,0 +1,15 @@
+(function( $ ) {
+
+    "use strict";
+
+    $(document).ready(function($) {
+
+        var data = {
+            'action': 'cilamp_ajax_action',
+        };
+
+        jQuery.post(cilamp_ajax.ajax_url, data, function(response) {
+            console.log('Got this from the server: ' + response);
+        });
+    });
+})(jQuery);
